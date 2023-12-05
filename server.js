54435b085e92c8d236f753aa8e51a0fe79eb8fa6
Michel Marion <kangkm19@gmail.com>
2023-12-04 20:58:09 -0500

    updated packages

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://pinvent2.netlify.app"],
+    origin: ["http://localhost:3000", "https://pinvent-app-kangkm.vercel.app"],
     credentials: true
 }));
 
